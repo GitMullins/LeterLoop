d2688cf211e46ea75f6e5b9cda588281a3535bba
David Mullins <Mullins6323@gmail.com>
2019-08-31 14:00:40 -0500

    wip

---
+++ LetterLoop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LetterLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Letter Loop");
+            var input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var output = input[i].ToString();
+                //var newOutput = output.ToString();
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
